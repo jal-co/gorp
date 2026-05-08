faad40b04c44280f7b8480f72a281b97f2d4da07
MaggieShan <sshan.maggie@gmail.com>
2026-05-08 17:30:18 -0400

    pr comments

--- crates/remote_server/build.rs
+++ crates/remote_server/build.rs
@@ -1,5 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/remote_server.proto");
-    prost_build::compile_protos(&["proto/remote_server.proto"], &["proto/"])?;
+    prost_build::compile_protos(
+        &["proto/remote_server.proto", "proto/diff_state.proto"],
+        &["proto/"],
+    )?;
     Ok(())
 }
